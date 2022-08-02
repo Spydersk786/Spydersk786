3a2d768e245784de58bf7d1c7a7bef9713e4ed29
Spydersk786 <101523497+Spydersk786@users.noreply.github.com>
2022-08-02 10:07:16 +0530

    Add files via upload

---
+++ assignment.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class assignment{
+	public static void main(String args[]){
+		System.out.println("enter the dimentions of matrix and its elements");
+        Scanner sc =new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int arr[][]=new int[n][m];
+        for(int i=0;i<n;i++)
+        {
+        	for(int j=0;j<m;j++)
+        		arr[i][j]=sc.nextInt();
+        }
+        int transpose[][]=new int[m][n];
+    	for(int i=0;i<n;i++)
+        {
+        	for(int j=0;j<m;j++)
+        		transpose[j][i]=arr[i][j];
+        }
+        for(int i=0;i<m;i++)
+        {
+        	for(int j=0;j<n;j++)
+        		System.out.print(transpose[i][j]+" ");
+        	System.out.println();
+        }
+	}
+}
