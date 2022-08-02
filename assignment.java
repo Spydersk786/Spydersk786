c17adb1fbc05ebe3752f20a6d0b62b44d00e5a0e
Spydersk786 <101523497+Spydersk786@users.noreply.github.com>
2022-08-02 11:46:28 +0530

    Add files via upload

---
+++ assignment.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class assignment{
+	public static void main(String args[]){
+		System.out.println("enter the length and elements of the array");
+        Scanner sc =new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+        		arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+        	for(int j=i+1;j<n;j++)
+        	{
+        		if(arr[i]>arr[j])
+        		{
+        			int temp=arr[i];
+        			arr[i]=arr[j];
+        			arr[j]=temp;
+        		}
+        	}
+        }
+        for(int i=0;i<n;i++)
+        {
+        		System.out.println(arr[i]+" ");
+        }
+	}
+}
