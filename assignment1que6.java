0e5e3f2cdabae7847f1af00080bcdadce4ed88e4
Spydersk786 <101523497+Spydersk786@users.noreply.github.com>
2022-08-02 12:01:12 +0530

    Update assignment1que6.java
    bubble sort

--- assignment1que6.java
+++ assignment1que6.java
@@ -7,7 +7,7 @@ public class assignment{
         int arr[]=new int[n];
         for(int i=0;i<n;i++)
         {
-        		arr[i]=sc.nextInt();
+        	arr[i]=sc.nextInt();
         }
         for(int i=0;i<n;i++)
         {
