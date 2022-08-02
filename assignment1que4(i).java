5e5cb29b2613abce276807dde21c85620b8efb7e
Spydersk786 <101523497+Spydersk786@users.noreply.github.com>
2022-08-02 08:46:36 +0530

    Add files via upload

---
+++ assignment1que4(i).java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class assignment1que4i{
+	public static void main(String args[]){
+		System.out.println("enter the number of elements in array and the elements");
+        Scanner sc =new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        	arr[i]=sc.nextInt();
+        for(int i=0;i<n/2;i++)
+        {
+        	int temp=arr[i];
+        	arr[i]=arr[n-i-1];
+        	arr[n-i-1]=temp;
+        }
+        for(int i=0;i<n;i++)
+        	System.out.println(arr[i]);
+	}
+}
